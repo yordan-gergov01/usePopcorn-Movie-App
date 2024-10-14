8693a88d58bbbe0862995f9d121ceb6646eec31c
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-14 17:22:44 +0300

    Clean-up data fetching and add abort controller

--- src/App.js
+++ src/App.js
@@ -80,11 +80,12 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError("");
-          const res = await fetch(url);
+          const res = await fetch(url, { signal: controller.signal });
 
           if (!res.ok)
             throw new Error("Something went wrong with fetching movies.");
@@ -92,9 +93,13 @@ export default function App() {
 
           if (data.Response === "False") throw new Error("Movie not found");
           setMovies(data.Search);
+          setError("");
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -106,6 +111,10 @@ export default function App() {
         return;
       }
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
