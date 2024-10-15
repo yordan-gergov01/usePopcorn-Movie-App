9033d8c3188a50c1425c9d66134379c896bcbd6e
yordan-gergov01 <yordansoftskills@gmail.com>
2024-10-15 13:35:21 +0300

    make components folder and final changes to finish the app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-// import StarRating from "./StarRating";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating maxRating={5} defaultRating={3} /> */}
   </React.StrictMode>
 );
